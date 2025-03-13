4001e846c0a55f9549e4f735480b59047dddcc20
Rossville <ashutosh.singh.9510@gmail.com>
2025-03-14 01:29:11 +0530

    Next and back btn working

--- main.js
+++ main.js
@@ -1,8 +1,30 @@
 var next_btn = document.getElementsByClassName('next-btn')[0];
 var circle_outlined_btn = document.getElementsByClassName('circle-outlined');
 var circle_solid = document.getElementsByClassName('circle-solid');
+var data_container = document.getElementsByClassName('data-container');
+var back_btn = document.getElementsByClassName('back-btn')[0];
+var formPages;
+(function (formPages) {
+    formPages[formPages["personal_info"] = 0] = "personal_info";
+    formPages[formPages["contact_info"] = 1] = "contact_info";
+    formPages[formPages["review_info"] = 2] = "review_info";
+})(formPages || (formPages = {}));
+var current_page = 0; //default
 next_btn.addEventListener('click', function () {
-    console.log("next btn clicked");
+    // moving to the next page upon next button click.
+    if (current_page < 2) {
+        data_container[current_page].classList.add('unactive');
+        data_container[current_page + 1].classList.remove('unactive');
+        current_page++;
+    }
+});
+back_btn.addEventListener('click', function () {
+    // moving to the previous page upon back button click.
+    if (current_page > 0) {
+        data_container[current_page].classList.add('unactive');
+        data_container[current_page - 1].classList.remove('unactive');
+        current_page--;
+    }
 });
 var _loop_1 = function (i) {
     circle_outlined_btn[i].addEventListener('click', function () {
--- main.ts
+++ main.ts
@@ -1,9 +1,33 @@
 const next_btn = document.getElementsByClassName('next-btn')[0];
 const circle_outlined_btn = document.getElementsByClassName('circle-outlined');
 const circle_solid = document.getElementsByClassName('circle-solid');
+const data_container = document.getElementsByClassName('data-container');
+const back_btn = document.getElementsByClassName('back-btn')[0];
+
+enum formPages{
+    personal_info = 0,
+    contact_info = 1,
+    review_info = 2
+}
+
+let current_page = 0; //default
 
 next_btn.addEventListener('click',function(){
-    console.log("next btn clicked");
+    // moving to the next page upon next button click.
+    if(current_page < 2){
+        data_container[current_page].classList.add('unactive');
+        data_container[current_page+1].classList.remove('unactive');
+        current_page++;
+    }
+})
+
+back_btn.addEventListener('click',function(){
+    // moving to the previous page upon back button click.
+    if(current_page>0){
+        data_container[current_page].classList.add('unactive');
+        data_container[current_page-1].classList.remove('unactive');
+        current_page--;
+    }
 })
 
 for(let i=0; i<circle_outlined_btn.length; i++){
