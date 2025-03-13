3ddcadeb8eb1b6c3201bd0c3f6c26eee9ac43ac2
Rossville <ashutosh.singh.9510@gmail.com>
2025-03-14 02:29:36 +0530

    Added: Hide back btn on first page, Submit btn appears on last page in place of next

--- main.js
+++ main.js
@@ -3,13 +3,26 @@ var circle_outlined_btn = document.getElementsByClassName('circle-outlined');
 var circle_solid = document.getElementsByClassName('circle-solid');
 var data_container = document.getElementsByClassName('data-container');
 var back_btn = document.getElementsByClassName('back-btn')[0];
-var formPages;
-(function (formPages) {
-    formPages[formPages["personal_info"] = 0] = "personal_info";
-    formPages[formPages["contact_info"] = 1] = "contact_info";
-    formPages[formPages["review_info"] = 2] = "review_info";
-})(formPages || (formPages = {}));
+var submit_btn = document.getElementsByClassName('submit-btn')[0];
+var submit_btn_container = document.getElementsByClassName('submit-btn-container')[0];
+var next_btn_container = document.getElementsByClassName('next-btn-container')[0];
+var back_btn_container = document.getElementsByClassName('back-btn-container')[0];
 var current_page = 0; //default
+function buttonChanges(curr_page) {
+    // check current_page and hide/disply submit and next btn
+    if (curr_page === 2) {
+        next_btn_container.classList.add('unactive');
+        submit_btn_container.classList.remove('unactive');
+    }
+    else {
+        next_btn_container.classList.remove('unactive');
+        submit_btn_container.classList.add('unactive');
+    }
+    if (curr_page !== 0)
+        back_btn_container.classList.remove('unactive');
+    else
+        back_btn_container.classList.add('unactive');
+}
 next_btn.addEventListener('click', function () {
     // moving to the next page upon next button click.
     if (current_page < 2) {
@@ -17,6 +30,7 @@ next_btn.addEventListener('click', function () {
         data_container[current_page + 1].classList.remove('unactive');
         current_page++;
     }
+    buttonChanges(current_page);
 });
 back_btn.addEventListener('click', function () {
     // moving to the previous page upon back button click.
@@ -25,6 +39,7 @@ back_btn.addEventListener('click', function () {
         data_container[current_page - 1].classList.remove('unactive');
         current_page--;
     }
+    buttonChanges(current_page);
 });
 var _loop_1 = function (i) {
     circle_outlined_btn[i].addEventListener('click', function () {
--- main.ts
+++ main.ts
@@ -3,14 +3,30 @@ const circle_outlined_btn = document.getElementsByClassName('circle-outlined');
 const circle_solid = document.getElementsByClassName('circle-solid');
 const data_container = document.getElementsByClassName('data-container');
 const back_btn = document.getElementsByClassName('back-btn')[0];
+const submit_btn = document.getElementsByClassName('submit-btn')[0];
+const submit_btn_container = document.getElementsByClassName('submit-btn-container')[0];
+const next_btn_container = document.getElementsByClassName('next-btn-container')[0];
+const back_btn_container = document.getElementsByClassName('back-btn-container')[0];
 
-enum formPages{
-    personal_info = 0,
-    contact_info = 1,
-    review_info = 2
+
+let current_page: number = 0; //default
+
+function buttonChanges(curr_page: number){
+    // check current_page and hide/disply submit and next btn
+    if(curr_page === 2){
+        next_btn_container.classList.add('unactive');
+        submit_btn_container.classList.remove('unactive');
+    }
+    else{
+        next_btn_container.classList.remove('unactive');
+        submit_btn_container.classList.add('unactive');
+    }
+
+    if(curr_page !== 0)
+        back_btn_container.classList.remove('unactive');
+    else
+        back_btn_container.classList.add('unactive');
 }
-
-let current_page = 0; //default
 
 next_btn.addEventListener('click',function(){
     // moving to the next page upon next button click.
@@ -19,6 +35,7 @@ next_btn.addEventListener('click',function(){
         data_container[current_page+1].classList.remove('unactive');
         current_page++;
     }
+    buttonChanges(current_page);
 })
 
 back_btn.addEventListener('click',function(){
@@ -28,7 +45,9 @@ back_btn.addEventListener('click',function(){
         data_container[current_page-1].classList.remove('unactive');
         current_page--;
     }
+    buttonChanges(current_page);
 })
+
 
 for(let i=0; i<circle_outlined_btn.length; i++){
     circle_outlined_btn[i].addEventListener('click',function(){
