663d93d004a1348416fe0cbefd4d74040750ed4d
Rossville <ashutosh.singh.9510@gmail.com>
2025-03-16 02:46:24 +0530

    Added: localStorage, Improved dynamic styles for input fields

--- main.js
+++ main.js
@@ -8,7 +8,13 @@ var submit_btn_container = document.getElementsByClassName('submit-btn-container
 var next_btn_container = document.getElementsByClassName('next-btn-container')[0];
 var back_btn_container = document.getElementsByClassName('back-btn-container')[0];
 var highlight_txt = document.getElementsByClassName('highlight-txt');
+var labelline = document.getElementsByClassName('labelline');
+var NameInput = document.querySelector(".field-name input[type=\"text\"]");
+var ReviewHeader = document.getElementsByClassName('reviewheader')[0];
+var DateInput = document.querySelector('.field-DOB input[type="date"]');
+var GenderInput = document.querySelector('.field-gender select');
 var current_page = 0; //default
+// remove placholder from the input date
 function buttonChanges(curr_page) {
     // check current_page and hide/disply submit and next btn
     if (curr_page === 2) {
@@ -17,6 +23,7 @@ function buttonChanges(curr_page) {
         circle_solid[2].classList.remove('unactive');
         highlight_txt[2].style.color = 'tomato';
         circle_outlined[2].style.borderColor = 'tomato';
+        ReviewHeader.classList.remove('unactive');
     }
     else {
         // when curr_page = 0 or 1
@@ -25,6 +32,7 @@ function buttonChanges(curr_page) {
         circle_solid[2].classList.add('unactive');
         highlight_txt[2].style.color = 'black';
         circle_outlined[2].style.borderColor = 'black';
+        ReviewHeader.classList.add('unactive');
     }
     if (curr_page === 0) {
         back_btn_container.classList.add('unactive');
@@ -57,6 +65,7 @@ next_btn.addEventListener('click', function () {
         current_page++;
     }
     buttonChanges(current_page);
+    saveData();
 });
 back_btn.addEventListener('click', function () {
     // moving to the previous page upon back button click.
@@ -66,4 +75,72 @@ back_btn.addEventListener('click', function () {
         current_page--;
     }
     buttonChanges(current_page);
+});
+// remove background text which is getting overlaped from input field.
+var updateLinelabel = function (inputs) {
+    var label = inputs.nextElementSibling;
+    if (!label || !label.classList.contains('labelline'))
+        return;
+    if (inputs.value.trim() !== "") {
+        label.style.transform = 'translate(-5px, -20px) scale(0.8)';
+        label.style.backgroundColor = 'white';
+        label.style.padding = '3px';
+        label.style.zIndex = "100";
+    }
+    else {
+        label.style.transform = "";
+        label.style.backgroundColor = "";
+        label.style.padding = "";
+        label.style.zIndex = "";
+    }
+};
+NameInput.addEventListener('focus', function () { return updateLinelabel(NameInput); });
+NameInput.addEventListener('input', function () { return updateLinelabel(NameInput); });
+NameInput.addEventListener('blur', function () { return updateLinelabel(NameInput); });
+function saveData() {
+    //using local Storage to save the progress.
+    localStorage.setItem("userData", JSON.stringify({
+        Name: NameInput.value,
+        Date: DateInput.value,
+        Gender: GenderInput.value
+    }));
+}
+submit_btn.addEventListener('click', function () {
+    //retrieve data from localStorage.
+    var storedData = JSON.parse(localStorage.getItem("userData") || "{}");
+    console.log(storedData.Name);
+});
+document.addEventListener('DOMContentLoaded', function () {
+    //check if there are any data store in localStorage.
+    var storedData = JSON.parse(localStorage.getItem("userData") || "{}");
+    if (storedData.Name !== "" || storedData.Date !== "" || storedData.Gender !== "") {
+        NameInput.value = storedData.Name;
+        DateInput.value = storedData.Date;
+        GenderInput.value = storedData.Gender;
+    }
+    // check if the Inputfields are empty or filled.
+    if (NameInput.value === "") {
+        var label = NameInput.nextElementSibling;
+        label.classList.remove('unactive');
+    }
+    else {
+        var label = NameInput.nextElementSibling;
+        label.classList.add('unactive');
+    }
+    if (DateInput.value === "") {
+        var label = DateInput.nextElementSibling;
+        label.classList.remove('unactive');
+    }
+    else {
+        var label = DateInput.nextElementSibling;
+        label.classList.add('unactive');
+    }
+    if (GenderInput.value === "") {
+        var label = GenderInput.nextElementSibling;
+        label.classList.remove('unactive');
+    }
+    else {
+        var label = GenderInput.nextElementSibling;
+        label.classList.add('unactive');
+    }
 });
--- main.ts
+++ main.ts
@@ -8,8 +8,15 @@ const submit_btn_container = document.getElementsByClassName('submit-btn-contain
 const next_btn_container = document.getElementsByClassName('next-btn-container')[0]  as HTMLElement;
 const back_btn_container = document.getElementsByClassName('back-btn-container')[0]  as HTMLElement;
 const highlight_txt = document.getElementsByClassName('highlight-txt') as HTMLCollectionOf<HTMLElement>;
+const labelline = document.getElementsByClassName('labelline') as HTMLCollectionOf<HTMLElement>;
+const NameInput = document.querySelector(".field-name input[type=\"text\"]") as HTMLInputElement;
+const ReviewHeader = document.getElementsByClassName('reviewheader')[0] as HTMLElement;
+const DateInput = document.querySelector('.field-DOB input[type="date"]') as HTMLInputElement;
+const GenderInput = document.querySelector('.field-gender select') as HTMLSelectElement;
 
 let current_page: number = 0; //default
+
+// remove placholder from the input date
 
 function buttonChanges(curr_page: number){
     // check current_page and hide/disply submit and next btn
@@ -19,6 +26,7 @@ function buttonChanges(curr_page: number){
         circle_solid[2].classList.remove('unactive');
         highlight_txt[2].style.color = 'tomato';
         circle_outlined[2].style.borderColor = 'tomato';
+        ReviewHeader.classList.remove('unactive');
     }
     else{
         // when curr_page = 0 or 1
@@ -27,6 +35,7 @@ function buttonChanges(curr_page: number){
         circle_solid[2].classList.add('unactive');
         highlight_txt[2].style.color = 'black';
         circle_outlined[2].style.borderColor = 'black';
+        ReviewHeader.classList.add('unactive');
     }
 
     if(curr_page === 0){
@@ -62,6 +71,7 @@ next_btn.addEventListener('click',function(){
         current_page++;
     }
     buttonChanges(current_page);
+    saveData();
 })
 
 back_btn.addEventListener('click',function(){
@@ -72,4 +82,83 @@ back_btn.addEventListener('click',function(){
         current_page--;
     }
     buttonChanges(current_page);
+})
+
+// remove background text which is getting overlaped from input field.
+
+const updateLinelabel = (inputs: HTMLInputElement) => {
+    const label = inputs.nextElementSibling as HTMLElement;
+    if(!label || !label.classList.contains('labelline'))
+        return;
+    
+    if(inputs.value.trim() !== ""){
+        label.style.transform = 'translate(-5px, -20px) scale(0.8)';
+        label.style.backgroundColor = 'white';
+        label.style.padding = '3px';
+        label.style.zIndex = "100";
+    }
+    else{
+        label.style.transform = "";
+        label.style.backgroundColor = "";
+        label.style.padding = "";
+        label.style.zIndex = "";
+    }
+
+}
+
+NameInput.addEventListener('focus',() => updateLinelabel(NameInput));
+NameInput.addEventListener('input',() => updateLinelabel(NameInput));
+NameInput.addEventListener('blur',() => updateLinelabel(NameInput));
+
+function saveData(){
+    //using local Storage to save the progress.
+    localStorage.setItem(
+        "userData",
+        JSON.stringify({
+            Name: NameInput.value,
+            Date: DateInput.value,
+            Gender: GenderInput.value
+        })
+    )
+}
+
+submit_btn.addEventListener('click',function(){
+    //retrieve data from localStorage.
+    const storedData = JSON.parse(localStorage.getItem("userData") || "{}");
+    console.log(storedData.Name);
+})
+
+document.addEventListener('DOMContentLoaded',function(){
+    //check if there are any data store in localStorage.
+    const storedData = JSON.parse(localStorage.getItem("userData") || "{}");
+    if(storedData.Name !== "" || storedData.Date !== "" || storedData.Gender !== ""){
+        NameInput.value = storedData.Name;
+        DateInput.value = storedData.Date;
+        GenderInput.value = storedData.Gender;
+    }
+    // check if the Inputfields are empty or filled.
+    if(NameInput.value === ""){
+       const label = NameInput.nextElementSibling as HTMLElement;
+       label.classList.remove('unactive');
+    }
+    else{
+        const label = NameInput.nextElementSibling as HTMLElement;
+        label.classList.add('unactive');
+    }
+    if(DateInput.value === ""){
+       const label = DateInput.nextElementSibling as HTMLElement;
+       label.classList.remove('unactive');
+    }
+    else{
+        const label = DateInput.nextElementSibling as HTMLElement;
+        label.classList.add('unactive');
+    }
+    if(GenderInput.value === ""){
+       const label = GenderInput.nextElementSibling as HTMLElement;
+       label.classList.remove('unactive');
+    }
+    else{
+        const label = GenderInput.nextElementSibling as HTMLElement;
+        label.classList.add('unactive');
+    }
 })
